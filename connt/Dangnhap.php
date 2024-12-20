49e677f7532bc33e7d598616dc4d535a43161363
LDung1203 <167894192+LDung1203@users.noreply.github.com>
2024-12-21 01:46:42 +0700

    21/12/24

--- connt/Dangnhap.php
+++ connt/Dangnhap.php
@@ -10,13 +10,13 @@ if ($phone && $pass) {
     $sql = "SELECT * FROM user WHERE phone = '$phone'";
     $result = mysqli_query($conn, $sql);
 
-    $sql_admin = "SELECT * FROM admin_p WHERE phone = '$phone'";
+    $sql_admin = "SELECT * FROM `admin` WHERE phone = '$phone'";
     $result_admin = mysqli_query($conn, $sql_admin);
 
     // Kiểm tra nếu số điện thoại có tồn tại trong cơ sở dữ liệu
     if ((mysqli_num_rows($result) > 0)||(mysqli_num_rows($result_admin) > 0)) {
         $user = mysqli_fetch_assoc($result); // Lấy thông tin người dùng từ cơ sở dữ liệu
-
+        $admin = mysqli_fetch_assoc($result_admin);
         // Kiểm tra mật khẩu có khớp không
         if ($pass === $user['password']) { // Chú ý: nếu có sử dụng hash mật khẩu, dùng password_verify
             // Nếu mật khẩu đúng, chuyển hướng đến trang 
@@ -26,8 +26,8 @@ if ($phone && $pass) {
             </script>";
         } else if($pass === $admin['password']){
             echo "<script>
-                alert('Đăng nhập thành công!');
-                window.location.href = '../admin/index.php'; // Chuyển hướng đến trang chủ
+                alert('Đăng nhập thành công!, xin chào Admin');
+                window.location.href = '../admin/Trangchu.php'; // Chuyển hướng đến trang admin
             </script>";
         } else {
             echo "<script>
